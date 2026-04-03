834c1d4f06bcfc4968daca86c2fca849b107d459
Elena Zudina <elen.zudina@gmail.com>
2026-04-03 17:39:02 +0300

    Add layout.php and make folder partials with header.php and footer.php for future development

---
+++ views/layout.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Art Portal</title>
+            <!-- Bootstrap не использую -->
+            <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2M2w1T"
+             crossorigin="anonymous" -->
+             <link rel= "stylesheet" type ="text/css" href="public/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+
+    </head>
+    <body>
+        <header>
+            <h1>Art Portal</h1>
+            <nav class="one">
+            <ul class="topmenu">
+                <li><a href="#">Styles<i class="fa fa-angle-down"></i></a>
+                    <ul class="submenu">
+                        <?php
+                        Controller::AllStyle();
+                        ?>
+                    </ul>
+                </li>
+                <li><a href="testError">Info</a></li>
+                <li><a href="./">Homepage</a></li>
+                <li><a href="registerForm">Register</a></li>
+            </ul>
+        </nav>
+        </header>
+        <main class = "container">
+            <?php
+                if (isset($content)) {
+                    echo $content;
+                }
+                else {
+                    echo '<h2>Content is gone</h2>';
+                }
+            ?>
+        </main>
+        <hr>
+        <footer>
+             <p class = "footer-text">JKTV24 2026a. &copy</p>
+        </footer>
+    </body>
+</html>
