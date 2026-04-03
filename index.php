8040922ce92dac5b327a766c90b2c321b309cc68
Elena Zudina <elen.zudina@gmail.com>
2026-04-03 17:58:25 +0300

    Update folder references to controllers, models, and views

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 // Файл для запуска проекта
 session_start();
 include_once 'config/Database.php';
-require 'model/Style.php';
-require 'model/Paintings.php';
+require 'models/Style.php';
+require 'models/Paintings.php';
 // Добавляю этот код на следующем этапе
-//require 'model/Comments.php';
-//require 'model/Register.php';
+//require 'models/Comments.php';
+//require 'models/Register.php';
 
 // Убрала view из index.php - нужно добавить в контроллер
-//include_once 'view/paintings.php';
-//include_once 'view/comments.php';
+//include_once 'views/paintings.php';
+//include_once 'views/comments.php';
 
-include_once 'controller/Controller.php';
+include_once 'controllers/Controller.php';
 include_once 'route/routing.php';
 
 //На данной стадии реализации $response = null, так как в контроллере нет return, а только include. В дальнейшем нужно будет изменить контроллер, чтобы он возвращал строку, а не выводил ее на экран. И уже в index.php эту строку выводить на экран. Это позволит более гибко управлять выводом данных и отделить логику от представления.
