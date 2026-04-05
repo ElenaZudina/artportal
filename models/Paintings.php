46ec2be3d33e1b52fc0f1e0bce8806270579cbf9
Elena Zudina <elen.zudina@gmail.com>
2026-04-05 12:31:35 +0300

    Add Paintings and Styles models

---
+++ models/Paintings.php
@@ -0,0 +1,36 @@
+<?php
+class Paintings{
+    public static function getLast10Paintings() {
+        $query = "SELECT * FROM paintings ORDER BY id DESC LIMIT 10";
+        $db = new Database();
+        $arr = $db->getAll($query);
+        return $arr;
+    }
+
+    public static function getAllPaintings() {
+        $query = "SELECT * FROM paintings ORDER BY id DESC";
+        $db = new Database();
+        $arr = $db->getAll($query);
+        return $arr;
+    }
+
+    public static function getPaintingsByStyleID($id) {
+        $query = "SELECT * FROM paintings where style_id = ? ORDER BY id DESC";
+        $db = new Database();
+        $arr = $db->getAll($query, [$id]);
+        return $arr;
+    }
+
+     public static function getPaintingByID($id) {
+        $query = "SELECT paintings.*, styles.name AS style_name, artists.name as artist_name, users.username
+        FROM paintings
+        JOIN styles ON paintings.style_id = styles.id
+        JOIN artists ON paintings.artist_id = artists.id
+        JOIN users ON paintings.user_id = users.id
+        WHERE paintings.id = ? ";
+        $db = new Database();
+        $arr = $db->getOne($query, [$id]);
+        return $arr;
+    }
+}
+?>
---
+++ models/Styles.php
@@ -0,0 +1,10 @@
+<?php
+class Style {
+
+    public static function getAllStyles() {
+        $query = "SELECT * FROM styles" ;
+        $db = new Database();
+        $arr = $db->getAll($query);
+        return $arr;
+    }
+}
