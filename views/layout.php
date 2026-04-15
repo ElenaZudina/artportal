637255b0c6c48344ddcd789f0ec1455d13a0b319
ElenaZudina <elena.zudina@ivkhk.ee>
2026-04-15 08:55:45 +0300

    Update layout: update  base footer and fix integrity attributes

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Art Portal</title>
             <!-- Bootstrap 5.3 CSS -->
-             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
+             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
              <!-- Custom CSS -->
              <!-- <link rel= "stylesheet" type ="text/css" href="public/css/style.css"> -->
              <!-- Google Fonys -->
@@ -50,9 +50,10 @@
             ?>
         </main>
         <footer class="container text-center py-4">
-             <p class="mb-0">JKTV24 2026a. &copy</p>
+            <p class="mb-0">ArtPortal. Celebrating painters worldwide..</p>
+             <p class="mb-0">JKTV24 2026a. &copy All Rights Reserved</p>
         </footer>
         <!-- Bootstrap 5.3 JS Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
