6395fb0d75a33fff14c768393091ab34745a1e2a
ElenaZudina <elena.zudina@ivkhk.ee>
2026-04-16 09:58:30 +0300

    Separate global and custom styles into style.css and custom.css

--- views/layout.php
+++ views/layout.php
@@ -8,7 +8,8 @@
              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
              <!-- Custom CSS -->
              <link rel="stylesheet" type="text/css" href="public/css/style.css">
-             <!-- Google Fonys -->
+             <link rel="stylesheet" type="text/css" href="public/css/custom.css">
+             <!-- Google Fonts -->
             <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
     </head>
     <body class="d-flex flex-column min-vh-100">
